64376075b58c3e9e454815851eb802a093e906c4
Shariful Islam <sharifrony725@gmail.com>
2022-10-26 14:55:09 +0600

    learning c

---
+++ 01.intro/dataType.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int num = 100;
+float num2 = 45.5f;
+char name = 'S';
+printf("%d\n", num);
+printf("%f\n", num2);
+printf("%c", name);
+return 0;
+//Number
+    //Integer 444 Float 4.0
+//Text
+    //Character '%','a','A'
+//Boolean
+}
---
+++ 01.intro/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    printf("\t\t\t___________________________\n");
+    printf("\t\t\tName : Shariful Islam\n");
+    printf("\t\t\tEmail: Sharifrony725@gmail.com\n");
+    printf("\t\t\tINST : IUS\n");
+    printf("\t\t\tRoll : 1010136\n");
+    printf("\t\t\tPhone: 0159512445654\n");
+    printf("\t\t\t___________________________");
+
+    return 0;
+}
